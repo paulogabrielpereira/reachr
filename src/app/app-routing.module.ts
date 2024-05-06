0fa90daa934cec6fc1525dcc746c4cf76a4882f4
paulogabriel <paulogabrieldeoliveira2@gmail.com>
2024-05-06 17:34:04 -0300

    iniciando projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditarComponent } from './components/vagas/editar/editar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'vagas/editar', component : EditarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { EditarComponent } from './components/vagas/editar/editar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    EditarComponent
   ],
   imports: [
     BrowserModule,
