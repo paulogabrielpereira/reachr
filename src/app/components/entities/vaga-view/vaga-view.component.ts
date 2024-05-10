57d669e991f3ec736c44c1f64b6c9cd09a008770
paulogabriel <paulogabrieldeoliveira2@gmail.com>
2024-05-10 18:50:10 -0300

    criando formulário de vagas

--- src/app/components/entities/vaga-view/vaga-view.component.ts
+++ src/app/components/entities/vaga-view/vaga-view.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VagaViewComponent implements OnInit {
 
+  numDoc : Number = 12341;
+  author : string = 'Leandro Tabota';
+
   constructor() { }
 
   ngOnInit(): void {
